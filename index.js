1bf7e4409c290413ef9a118742caddaa93041f19
Kassandra <kassgallardo1@gmail.com>
2022-09-15 20:42:11 -0400

    class follow along

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//Your Code here
+let response = window.prompt("Do you go left or right?").toLowerCase();
+console.log(response);
+
+if(response === "left"){
+    //we run logic for left response.
+    console.log("user submitted left")
+
+    let response2 = window.prompt("Do you go down or up?")
+        if(response2 === "up"){console.log ("user submitted up")}
+} else if (response === "right"){
+    //we run logic for right response.
+    console.log("user submitted right")
+} else {
+    //show error or ask question again.
+}
